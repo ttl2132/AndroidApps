fd67f925e04cd828e58998684ec08e9a44f3981b
chiyo <chiyo-chan@hotmail.com>
2018-12-28 02:14:28 -0500

    Starting to add buttons

---
+++ GeoQuiz/app/src/main/java/com/bigcoderanch/android/geoquiz/QuizActivity.java
@@ -0,0 +1,34 @@
+package com.bigcoderanch.android.geoquiz;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+public class QuizActivity extends AppCompatActivity {
+    private Button mTrueButton;
+    private Button mFalseButton;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_quiz);
+
+        mTrueButton = (Button) findViewById(R.id.true_button);
+        mFalseButton = (Button) findViewById(R.id.false_button);
+        mTrueButton.setOnClickListener(new
+                View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+
+                    }
+                });
+        mFalseButton.setOnClickListener(new
+            View.OnClickListener() {
+                @Override
+                public void onClick (View v) {
+
+                }
+            });
+    }
+}
